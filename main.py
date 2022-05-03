f11c6cccd3d02d9c8e529117540081ec466e70d0
DomZeman <98137536+DomZeman@users.noreply.github.com>
2022-05-03 14:31:49 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -13,18 +13,6 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GroupKFold
 
-label_dict = {0: 'air conditioner',
-              1: 'car horn',
-              2: 'children playing',
-              3: 'dog bark',
-              4: 'drilling',
-              5: 'engine idling',
-              6: 'gun shot',
-              7: 'jackhammer',
-              8: 'siren',
-              9: 'street music'
-              }
-
 audio_dataset_path = 'UrbanSound8K/audio/'
 metadata = pd.read_csv('UrbanSound8K/metadata/UrbanSound8K.csv')
 
@@ -33,10 +21,7 @@ def features_extractor(file):
     audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
     mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
     mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
-    #mel_features = librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels=40, fmax=8000)
-    #mel_scaled_features = np.mean(mel_features.T, axis=0)
     return mfccs_scaled_features
-
 
 extracted_features = []
 
@@ -82,9 +67,8 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
 
-num_epochs = 100
-num_batch_size = 32
-#num_batch_size = 256
+num_epochs = 500 #100 originally
+num_batch_size = 256 #32 originally
 
 checkpointer = ModelCheckpoint(filepath='saved_models/audio_classification.hdf5', verbose=1, save_best_only=True)
 
@@ -92,6 +76,12 @@ group_kfold = GroupKFold(n_splits=10)
 group_kfold.get_n_splits(X, y, folds)
 
 accuracies = []
+
+high_acc = 0
+high_X_train = []
+high_X_test = []
+high_y_train = []
+high_y_test = []
 
 for train_index, test_index in group_kfold.split(X, y, folds):
     X_train, X_test = X[train_index], X[test_index]
@@ -103,4 +93,15 @@ for train_index, test_index in group_kfold.split(X, y, folds):
     print(test_accuracy[1])
     accuracies.append(test_accuracy[1])
 
+    if test_accuracy[1] > high_acc:
+        high_acc = test_accuracy[1]
+        high_X_train = X_train
+        high_X_test = X_test
+        high_y_train = y_train
+        high_y_test = y_test
+
+model.fit(high_X_train, high_y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(high_X_test, high_y_test), callbacks=[checkpointer], verbose=1)
+
+
 print("accuracies of 10-fold cross validation: " + str(accuracies))
+print("highest accuracy: " + str(high_acc))
