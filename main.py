a5275c30e9db1146efc7841bb38b0abbd493ac6b
DomZeman <98137536+DomZeman@users.noreply.github.com>
2022-05-11 17:50:15 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GroupKFold
 
+#File Paths
 audio_dataset_path = 'UrbanSound8K/audio/'
 metadata = pd.read_csv('UrbanSound8K/metadata/UrbanSound8K.csv')
 
-
+#Feature Extraction Function
 def features_extractor(file):
     audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
     mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
     mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
     return mfccs_scaled_features
 
+#Feature Extraction
 extracted_features = []
 
 for index_num, row in tqdm(metadata.iterrows()):
@@ -32,7 +34,8 @@ for index_num, row in tqdm(metadata.iterrows()):
     data = features_extractor(file_name)
     extracted_features.append([data, final_class_labels, final_class_folds])
 
-extracted_features_df=pd.DataFrame(extracted_features,columns=['feature','class', 'fold'])
+#Make the Dataframe
+extracted_features_df = pd.DataFrame(extracted_features,columns=['feature','class', 'fold'])
 extracted_features_df.head()
 
 X = np.array(extracted_features_df['feature'].tolist())
@@ -72,6 +75,7 @@ num_batch_size = 256 #32 originally
 
 checkpointer = ModelCheckpoint(filepath='saved_models/audio_classification.hdf5', verbose=1, save_best_only=True)
 
+#10-Fold Cross Validation
 group_kfold = GroupKFold(n_splits=10)
 group_kfold.get_n_splits(X, y, folds)
 
@@ -100,6 +104,7 @@ for train_index, test_index in group_kfold.split(X, y, folds):
         high_y_train = y_train
         high_y_test = y_test
 
+#Fitting of the Most Accurate Combination
 model.fit(high_X_train, high_y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(high_X_test, high_y_test), callbacks=[checkpointer], verbose=1)
 
 
